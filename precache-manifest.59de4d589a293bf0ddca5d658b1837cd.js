c885047ebd9025de0db0c59043a482dc2f5f9192
Arimazz <bodyaprymyak@icloud.com>
2020-02-17 15:43:05 +0200

    Updates

---
+++ precache-manifest.59de4d589a293bf0ddca5d658b1837cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "353eb1267e7c058fbdda4fb74454dd59",
+    "url": "/react_movies-list-filter/index.html"
+  },
+  {
+    "revision": "8e4125a4efb06acca102",
+    "url": "/react_movies-list-filter/static/css/2.70d9d258.chunk.css"
+  },
+  {
+    "revision": "4adca99267e7b7c62aae",
+    "url": "/react_movies-list-filter/static/css/main.bada76a3.chunk.css"
+  },
+  {
+    "revision": "8e4125a4efb06acca102",
+    "url": "/react_movies-list-filter/static/js/2.8d572d85.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react_movies-list-filter/static/js/2.8d572d85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4adca99267e7b7c62aae",
+    "url": "/react_movies-list-filter/static/js/main.12b10b34.chunk.js"
+  },
+  {
+    "revision": "3fa16bf69277d642f8ea",
+    "url": "/react_movies-list-filter/static/js/runtime-main.54751719.js"
+  }
+]);
